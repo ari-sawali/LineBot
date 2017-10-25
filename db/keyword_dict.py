9ee7f24dc73f412e3b690788efa3ab348b03e313
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-10-26 03:36:21 +0800

    debug

--- db/keyword_dict.py
+++ db/keyword_dict.py
@@ -168,7 +168,7 @@ class group_dict_manager(db_base):
             or_list = [{ pair_data.AFFILIATED_GROUP: self._group_id }, { pair_data.AFFILIATED_GROUP: PUBLIC_GROUP_ID }]
 
             if '$or' in args[0]:
-                args[0]['$and'] = [{'$or': or_list}, {'$or': filter['$or']}]
+                args[0]['$and'] = [{'$or': or_list}, {'$or': args[0]['$or']}]
                 del args[0]['$or']
             else:
                 args[0]['$or'] = or_list
