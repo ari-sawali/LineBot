149483aaa4ba69fe95074d2aa2c067491e3d83e7
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-10-27 22:55:44 +0800

    debug

--- bot/msg_handler/text_msg.py
+++ bot/msg_handler/text_msg.py
@@ -597,7 +597,7 @@ class text_msg_handler(object):
                 text = u'UID:\n{}\n名稱:\n{}\n擁有權限:\n{}\n製作回覆組ID:\n{}'.format(uid, name, owned_permission, created_id_arr)
 
                 return [bot.line_api_wrapper.wrap_text_message(text, self._webpage_generator), 
-                        bot.line_api_wrapper.wrap_template_with_action({ '查詢該使用者製作的回覆組': text_msg_handler.HEAD + text_msg_handler.SPLITTER + 'Q' + text_msg_handler.SPLITTER + 'ID' + text_msg_handler.SPLITTER + '  '.join(created_id_arr) }, u'回覆組製作查詢快捷樣板', u'快捷查詢')]
+                        bot.line_api_wrapper.wrap_template_with_action({ '查詢該使用者製作的回覆組': text_msg_handler.HEAD + text_msg_handler.SPLITTER + 'Q' + text_msg_handler.SPLITTER + 'UID' + text_msg_handler.SPLITTER + uid }, u'回覆組製作查詢快捷樣板', u'快捷查詢')]
             else:
                 return wrong_param1
 
