350e0fbd929522a57d7ba7f82dd5d163072e19fa
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-10-28 17:58:22 +0800

    debug

--- app.py
+++ app.py
@@ -172,8 +172,8 @@ def callback():
 
     # handle webhook body
     try:
-        # handler.handle(body, signature)
-        handle_pool.apply(handler.handle, args=(body, signature))
+        handler.handle(body, signature)
+        # handle_pool.apply(handler.handle, args=(body, signature))
     except exceptions.InvalidSignatureError:
         abort(400)
 
