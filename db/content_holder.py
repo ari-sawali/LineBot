68d0f702cc7bc09a8b4cc429a71bb11e99bb2b5c
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-10-30 04:58:10 +0800

    debug

--- db/content_holder.py
+++ db/content_holder.py
@@ -205,9 +205,15 @@ class rps_holder(db_base):
             { '$match': {
                 rps_online.CHAT_INSTANCE_ID: cid,
                 '$or': [
-                    { rps_online.PLAYERS + '.' + battle_player.USER_ID: uid }, 
-                    { rps_online.PLAYERS + '.' + battle_player.USER_ID: rps_at_local.temp_uid_1 }
+                    { rps_online.PLAYERS + '.' + uid + '.' + battle_player.USER_ID: uid }, 
+                    { rps_online.PLAYERS + '.' + rps_at_local.temp_uid_1 + '.' + battle_player.USER_ID: rps_at_local.temp_uid_1 }
                 ]
+            } },
+            { '$replaceRoot': { 
+                'newRoot': '$' + rps_online.PLAYERS
+            } },
+            { '$sort': { 
+                battle_player.USER_ID: pymongo.DESCENDING if uid > rps_at_local.temp_uid_1 else pymongo.ASCENDING
             } }
         ]))
 
