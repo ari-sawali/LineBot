4ca81be58949f0f9375477d6f078667fe34fb58c
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-11-04 20:05:55 +0800

    debug

--- bot/msg_handler/text_msg.py
+++ bot/msg_handler/text_msg.py
@@ -831,6 +831,7 @@ class text_msg_handler(object):
                 'U': bot.system_data_category.LAST_UID
             }
 
+            print len(self._array_separator)
             last_item_cat = category_dict.get(category)
 
             if last_item_cat is None:
@@ -841,7 +842,7 @@ class text_msg_handler(object):
             rep_list = []
 
             if last_array is not None and len(last_array) > 0:
-                rep_list.append(bot.line_api_wrapper.wrap_text_message(u'{}\n{}。'.format(unicode(last_item_cat), u'、'.join([str(item) for item in last_array])), self._webpage_generator))
+                rep_list.append(bot.line_api_wrapper.wrap_text_message(u'{} (右邊較新)\n{}。'.format(unicode(last_item_cat), u'、'.join([str(item) for item in last_array])), self._webpage_generator))
             else:
                 return error.main.miscellaneous(u'沒有登記到本頻道的{}，有可能是因為機器人重新啟動而造成。\n\n本次開機時間: {}'.format(unicode(last_item_cat), self._system_data.boot_up))
 
