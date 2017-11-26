36323cab7d4f9d85a228164208e1f423378a0e25
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-11-27 00:56:48 +0800

    debug

--- bot/system.py
+++ bot/system.py
@@ -162,8 +162,9 @@ class infinite_loop_prevent_data(object):
 
         self._message_record.append(new_data)
 
-        print new_data
-        print last_data
+        print new_data.__dict__
+        print last_data.__dict__
+        print self._repeat_count
         if new_data == last_data:
             self._repeat_count += 1
         else:
