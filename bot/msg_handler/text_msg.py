d4175fc024deb2c958a47bf7e8d64a4b1fbd6a7c
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-12-27 03:57:31 +0800

    debug

--- bot/msg_handler/text_msg.py
+++ bot/msg_handler/text_msg.py
@@ -130,7 +130,7 @@ class text_msg_handler(object):
         for cmd_obj in bot.sys_cmd_dict.itervalues():
             for header in cmd_obj.headers:
                 s = text.split(u'\n')
-                match_en = s[0] == text_msg_handler.EN_HEAD and s[1] == header.replace(u'\n', u'')
+                match_en = s[0] == text_msg_handler.EN_HEAD.replace(u'\n', u'') and s[1] == header.replace(u'\n', u'')
                 if text.startswith(text_msg_handler.CH_HEAD + header) or match_en:
                     return cmd_obj
 
